33c76abcd73156f927f82bbc541221b10a5246aa
Jezegou Pierre-Yves <salydu29@gmail.com>
2020-01-20 10:39:36 +0100

    inited Maven Project

---
+++ src/main/java/fr/piwithy/PiMonteCarloMth.java
@@ -0,0 +1,14 @@
+package fr.piwithy;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class PiMonteCarloMth {
+
+    public static Logger LOGGER = LogManager.getLogger(PiMonteCarloMth.class);
+
+    public static void main(String[] args){
+        LOGGER.info("Hello World!");
+    }
+
+}
