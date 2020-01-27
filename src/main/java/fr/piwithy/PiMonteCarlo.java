45977865d9430272bfde649bac6c32219b1b0169
PIerre-YVes JEZEGOU <salydu29@gmail.com>
2020-01-27 12:09:42 +0100

    commit bis

--- src/main/java/fr/piwithy/PiMonteCarlo.java
+++ src/main/java/fr/piwithy/PiMonteCarlo.java
@@ -1,4 +1,55 @@
 package fr.piwithy;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.concurrent.*;
+
 public class PiMonteCarlo {
+
+    private static Logger LOGGER = LogManager.getLogger(PiMonteCarlo.class);
+
+
+    public static void main(String[] args){
+        int niter = 2147483647;
+        int nThread=8;
+        int count=0;
+        double pi;
+        Instant st = Instant.now();
+        ExecutorService executor = Executors.newFixedThreadPool(nThread);
+        ArrayList<Callable<Integer>> callables = new ArrayList<>();
+        for(int i=0; i<nThread; i++){
+            callables.add(new PiThread(niter/nThread, i+1));
+        }
+        try {
+            count = executor.invokeAll(callables).stream().map(integerFuture -> {
+                try {
+                    return integerFuture.get();
+                } catch (Exception e) {
+                    throw new IllegalStateException(e);
+                }
+            }).mapToInt(Integer::intValue).sum();
+            pi = (double) count / niter * 4;
+            Instant end = Instant.now();
+            long timeMs = Duration.between(st, end).toMillis();
+            LOGGER.info("estimate of pi: " + pi + " | Duration " + timeMs + "ms");
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+        try{
+            executor.shutdown();
+            executor.awaitTermination(5, TimeUnit.SECONDS);
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }finally {
+            if(!executor.isTerminated()){
+                LOGGER.fatal("Canceling non-finished task!");
+            }
+            executor.shutdownNow();
+        }
+    }
 }
--- src/main/java/fr/piwithy/PiThread.java
+++ src/main/java/fr/piwithy/PiThread.java
@@ -1,10 +1,51 @@
 package fr.piwithy;
 
-import java.util.concurrent.Callable;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
-public class Pithread implements Callable {
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Random;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class PiThread implements Callable<Integer> {
+
+    private Logger logger;
+
+    private int nIter,tID;
+
+
+    public PiThread(int nIter, int tID) {
+        this.nIter = nIter;
+        this.tID = tID;
+        this.logger=LogManager.getLogger("PiDart Thread " + tID);
+    }
+
+    public PiThread(int nIter){
+        this(nIter, ThreadLocalRandom.current().nextInt());
+    }
+
+    public int piDarts(){
+        int count=0;
+        double x,y,z;
+        for(int i=0;i<nIter;i++){
+            x= ThreadLocalRandom.current().nextDouble(0,1);
+            y= ThreadLocalRandom.current().nextDouble(0,1);
+            z=x*x+y*y;
+            if(z<=1) count++;
+        }
+        return count;
+    }
+
     @Override
-    public Object call() throws Exception {
-        return null;
+    public Integer call() throws Exception {
+        logger.debug("This is Thread: " +this.tID+" ; Working on "+ this.nIter + " iteration");
+        Instant st = Instant.now();
+        int cnt = piDarts();
+        Instant end = Instant.now();
+        long time_ms= Duration.between(st, end).toMillis();
+        logger.debug("Computing Finished: " + cnt+ " duration " + time_ms+ "ms");
+        return cnt;
     }
 }
